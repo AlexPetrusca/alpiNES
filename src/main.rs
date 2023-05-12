6da214024f5185428d0eb6791624c6989939af65
AlexPetrusca <petruscalex@gmail.com>
2023-05-12 12:33:03 -0700

    remove rom log

--- src/main.rs
+++ src/main.rs
@@ -232,10 +232,10 @@ fn main() {
     // run_game("rom/test/ppu/240pee.nes");
     // run_game("rom/test/apu/sndtest.nes");
 
-    // run_game("rom/mapper0/pacman.nes");
+    run_game("rom/mapper0/pacman.nes");
     // run_game("rom/mapper1/legend_of_zelda.nes"); // todo: impl
     // run_game("rom/mapper2/contra.nes");
-    run_game("rom/mapper3/arkistas_ring.nes");
+    // run_game("rom/mapper3/arkistas_ring.nes");
     // run_game("rom/mapper4/super_mario_bros_3.nes"); // todo: impl
     // run_game("rom/mapper5/castlevania_3.nes"); // todo: impl
     // run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
