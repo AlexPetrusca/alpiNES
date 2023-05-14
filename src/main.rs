89a8188db68f64fb9f29795eb0983eeabca9896b
AlexPetrusca <petruscalex@gmail.com>
2023-05-13 17:06:37 -0700

    comment line

--- src/main.rs
+++ src/main.rs
@@ -236,7 +236,7 @@ fn main() {
     // run_game("rom/mapper3/bump_n_jump.nes");
     // run_game("rom/mapper4/super_mario_bros_3.nes"); // todo: impl
     // run_game("rom/mapper5/castlevania_3.nes"); // todo: impl
-    run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
+    // run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
 
     /* TODO | regression test plan - run each game after changes | TODO */
     // run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
