0e02587f19af2607ba395b5140e9c15bf2817970
AlexPetrusca <petruscalex@gmail.com>
2023-05-13 17:03:30 -0700

    add regressions

--- src/main.rs
+++ src/main.rs
@@ -237,4 +237,9 @@ fn main() {
     // run_game("rom/mapper4/super_mario_bros_3.nes"); // todo: impl
     // run_game("rom/mapper5/castlevania_3.nes"); // todo: impl
     run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
+
+    /* TODO | regression test plan - run each game after changes | TODO */
+    // run_game("rom/mapper66/super_mario_bros_duck_hunt.nes");
+    // run_game("rom/mapper0/ice_climber.nes");
+    run_game("rom/mapper3/arkistas_ring.nes");
 }
